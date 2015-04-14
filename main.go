a93ebff095877d8ad3f6c8fa7dc6a6be35d5b65c
Chris Saunders <chris.saunders@shopify.com>
2015-04-14 16:12:10 +0000

    Adding gifs

--- main.go
+++ main.go
@@ -2,22 +2,12 @@ package main
 
 import "net/http"
 
-func deal(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, dealUrl, http.StatusMovedPermanently)
-}
-
-func magic(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, magicUrl, http.StatusMovedPermanently)
-}
-
 func index(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(indexHtml))
 }
 
 func main() {
 	http.HandleFunc("/", index)
-	http.HandleFunc("/magic", magic)
-	http.HandleFunc("/deal", deal)
 	http.ListenAndServe(":8000", nil)
 }
 
@@ -30,8 +20,8 @@ const indexHtml string = `
   <title>Demo Go App</title>
 </head>
 <body>
-  <p><a href="/magic">See some Magic</a></p>
-  <p><a href="/deal">Not a fan?</a></p>
+  <p><a href="/magic.gif">See some Magic</a></p>
+  <p><a href="/deal.gif">Not a fan?</a></p>
 </body>
 </html>
 `
